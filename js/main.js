7f21d505cbe6de67070cbd5ad58c6c3a7ea316e5
I.D.Als <Als@IDAlstekiMacBook-Air.local>
2013-11-07 18:12:06 +0800

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".sa-rant").click(function(){
+        $(".show_area").load("load/sa-rant.html",function(responseTxt,statusTxt,xhr){
+            if(statusTxt=="success"){
+                var a=$(".show_area");
+                a.innerHTML=statusTxt;}
+            else var txt=xhr.status+": "+xhr.statusText;
+            a.innerHTML=txt;
+        })
+})
+})
