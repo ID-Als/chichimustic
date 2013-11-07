e5e96556690b5afd53b6726506a3f6fe1433d0c6
Als <416444735@qq.com>
2013-11-07 19:14:32 +0800

    new index

--- js/main.js
+++ js/main.js
@@ -3,9 +3,12 @@ $(document).ready(function(){
         $(".show_area").load("load/sa-rant.html",function(responseTxt,statusTxt,xhr){
             if(statusTxt=="success"){
                 var a=$(".show_area");
-                a.innerHTML=statusTxt;}
-            else var txt=xhr.status+": "+xhr.statusText;
-            a.innerHTML=txt;
+                a.innerHTML=statusTxt;
+            }
+            else {
+                var txt=xhr.status+": "+xhr.statusText;
+                a.innerHTML=txt;
+            }
         })
-})
+    })
 })
