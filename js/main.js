448e5f3ca4e6989f4225a3ac15e383f74ea0879e
Als <416444735@qq.com>
2013-11-09 11:26:06 +0800

    a try

--- js/main.js
+++ js/main.js
@@ -1,18 +1,81 @@
 $(document).ready(function(){
-    $("#show_area").load("load/rant.html",function(responseTxt,statusTxt,xhr){
-                var a=$("#show_area");
-                if(statusTxt=="success"){a.innerHTML=statusTxt;}
-                else {a.innerHTML=xhr.status+": "+xhr.statusText;}
-    })
-    $(".sa-rant").each(function(){
-        $(this).click(function(event){
-            $("#show_area").load($(event.target).find("p.label").attr("href"),function(responseTxt,statusTxt,xhr){
-                    var a=$("#show_area");
-                    if(statusTxt=="success"){a.innerHTML=statusTxt;}
-                    else {a.innerHTML=xhr.status+": "+xhr.statusText;}
-})})})})
+    var ajaxstate="normal";
+    var navlink=$(".sa-nav");
+    var used_list=Array();
+    navlink.update=function(urlx){
+        var strs="";
+        for(var i= 0;i<used_list.length;i++){
+            strs+="<span class='annoymous'>></span>"+"<a style='text-decoration:none;color:#b3d4fc;'>"+ used_list[i]+'</a>'
+        }
+        navlink.html(strs);
+    }
 
 
+
+
+
+
+    function buffering(){
+        if(ajaxstate!="normal"){
+            $("#show_area").fadeOut(100,function(){
+                $("buffer-img").css("display","block")
+            })
+        }
+        else{$("buffer-img").css("display","none")}
+    }
+    setInterval(buffering,1000);
+    function loadx(urlx,elm){
+        $("#show_area").load(urlx,function(responseTxt,statusTXT){
+            if(statusTXT=="success"){
+
+
+
+                if(elm.attr("alt"))var xx=elm.attr("alt");
+                else var xx=elm.text();
+                alert(xx);
+                concat(used_list,(xx));//图片的文字为alt属性，《p》即文字。
+                alert(elm.attr("alt"));
+                alert(used_list[0].name)
+                navlink.update(urlx);
+
+
+
+                ajaxstate="normal";
+                $(".sa-bind-click").each(function(i,ss){//在图片和文字说明上添加class“sa-bind-click”，可以从此绑定handler；
+                    $(ss).bind("click",function(){loadx($(ss).attr("href"));});
+                })
+            }
+            else {ajaxstate="abnormal";}
+        })
+    }
+    loadx("load/rant.html",$("#show_area"))
+})
+
+    /*$("#show_area").load("load/rant.html",function(){
+        navlink.append("<span class='sa-nav-link'>>"+"餐厅列表"+"</span>")
+
+        $(".sa-rant").each(function(){$(this).click(function(event){
+        if($(event.target).attr("class")=="sa-rant-img")var ss=$(event.target).next();
+        if($(event.target).attr("class")=="sa-rant")var ss=$(event.target).find("p.label");
+        if($(event.target).attr("class")=="label") var ss=$(event.target);
+
+        $("#show_area").load(ss.attr("href"),function(){
+            navlink.append("<span class='sa-nav-link'>>"+ss.text()+"</span>")
+
+            $(".sa-food").each(function(){$(this).click(function(event){
+            if($(event.target).attr("class")=="sa-food-img")var ss=$(event.target).next();
+            if($(event.target).attr("class")=="sa-food")var ss=$(event.target).find("p.label");
+            if($(event.target).attr("class")=="label") var ss=$(event.target);
+            $("#show_area").load($(ss).attr("href"),function(responseTxt,statusTXT,xhr){
+                if(statusTXT=="success"){navlink.append("<span class='sa-nav-link'>>"+ss.text()+"</span>")}
+
+            });
+        })})})
+    })})})
+*/
+
+
+/*罗拉快跑*/
 
 
 
