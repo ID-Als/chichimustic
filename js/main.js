6947b5bd6e22ffe13de10dc9cadc90f747bfc5a8
Als <416444735@qq.com>
2013-11-10 10:36:33 +0800

    buffer

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 $(document).ready(function(){
     var ajaxstate="normal";
-
-    function LinkUpdate(urlx,elm) {
-        var x=elm.attr("alt")
-            if(elm.attr("class")=="navlink sa-bind-click"){
-                elm.nextAll().each(function(){this.remove();alert("removed1")});
-                elm.remove();alert("removed2");;elm.prevAll().each(function(){
-                    this.remove();
-                });alert("removed3");
-
-            }
-        var strings = "<span class='anonymous'>&gt</span> <span class='navlink sa-bind-click'"
-                    +" style='text-decoration:none;color:#b3d4fc;' alt="+ x +" href="
-                    + urlx + ">" + x + '</span>';
-        $(".sa-nav").append(strings);
-    };
-
     function buffering(){
         if(ajaxstate!="normal"){
             $("#show_area").fadeOut(100,function(){
@@ -24,14 +8,10 @@ $(document).ready(function(){
         else{$(".buffer-img").css("display","none")}
     }
     setInterval(buffering,500);
-
     function loadx(urlx,elm){
         $("#show_area").load(urlx,function(responseTxt,statusTXT){
             if(statusTXT=="success"){
                 ajaxstate="normal";
-                alert("xxx")
-                LinkUpdate(urlx,elm)
-                alert("xxx-")
                 $(".sa-bind-click").each(function(i,ss){//添加class“sa-bind-click”，可以从此绑定handler；
                     $(ss).bind("click",function(){loadx($(ss).attr("href"),$(ss));});})
             }
@@ -40,8 +20,24 @@ $(document).ready(function(){
     loadx("load/rant.html",$("#show_area"))
 })
 
+//LinkUpdate(urlx,elm)
+/*function LinkUpdate(urlx,elm) {
+        var x=elm.attr("alt")
+        if(elm.attr("class")=="navlink sa-bind-click"){
+            elm.nextAll().each(function(){this.remove();alert("removed1")});
+            elm.remove();alert("removed2");;elm.prevAll().each(function(){
+                this.remove();
+            });alert("removed3");
+        }
+        var xxx=$(".sa-nav");
+        var strings=xxx.innerHTML;
+        xxx.innerHTML="";
 
-
+        strings += "<span class='anonymous'>&gt</span> <span class='navlink sa-bind-click'"
+                    +" style='text-decoration:none;color:#b3d4fc;' alt="+ x +" href="
+                    + urlx + ">" + x + '</span>';
+        xxx.append(strings);
+    };*/
 /*var saApp=angular.module("saApp",[])
 
 saApp.controller('saCtrl', ['$scope', function ($scope) {
